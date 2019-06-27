e9a404c1de7a4ba76b9c666c960d3a50426f45cf
paullew94 <paullew@hotmail.com>
2019-06-27 10:46:00 -0300

    Repositorio cliente finalizado

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Clientes
+    public class Cliente
     {
         public int Id;
         public string Nome;
--- Repository/ClienteRepositorio.cs
+++ Repository/ClienteRepositorio.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
 {
-    class ClientesRepositorio
+    public class ClienteRepositorio
     {
+        public string CadeiaConexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\contas.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public void Inserir(Cliente cliente)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexao;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"INSERT INTO cliente (nome,cpf,data_nascimento,rg) VALUES (@NOME,@CPF,@DATA_NASCIMENTO,@RG)";
+            comando.Parameters.AddWithValue("@NOME", cliente.Nome);
+            comando.Parameters.AddWithValue("@CPF", cliente.Cpf);
+            comando.Parameters.AddWithValue("@DATA_NASCIMENTO", cliente.DataNascimento);
+            comando.Parameters.AddWithValue("@RG", cliente.Rg);
+            comando.ExecuteNonQuery();
+            conexao.Close();
+
+        }
+
+        public List<Cliente> ObterTodos(string busca)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexao;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"SELECT * FROM clientes WHERE nome LIKE @NOME";
+            busca = "%" + busca + "%";
+            comando.Parameters.AddWithValue("@NOME", busca);
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            conexao.Close();
+
+            List<Cliente> clientes = new List<Cliente>();
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                DataRow linha = tabela.Rows[i];
+                Cliente cliente = new Cliente();
+                cliente.Id = Convert.ToInt32(linha["id"]);
+                cliente.Nome = linha["nome"].ToString();
+                cliente.Cpf = linha["cpf"].ToString();
+                cliente.DataNascimento=Convert.ToDateTime(linha[])
+
+            }
+
+        }
     }
 }
